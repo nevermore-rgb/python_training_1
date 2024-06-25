03b450599d86c5644dcc4a8eb44481ff9ba3ef54
nevermore-rgb <58877861+nevermore-rgb@users.noreply.github.com>
2024-06-25 21:52:11 +0300

    Delete Day2/main.py

--- Day2/main.py
+++
@@ -1,9 +0,0 @@
-import my_package.math_utils
-
-a = 10
-b = 5
-
-print(f"Addition: {my_package.math_utils.add(a, b)}")        
-print(f"Subtraction: {my_package.math_utils.subtract(a, b)}")  
-print(f"Multiplication: {my_package.math_utils.multiply(a, b)}")  
-print(f"Division: {my_package.math_utils.divide(a, b)}") 
