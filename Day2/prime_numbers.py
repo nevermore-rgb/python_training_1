de3a0cbce8eb603209e704f332d7f1f4861e378c
nevermore-rgb <58877861+nevermore-rgb@users.noreply.github.com>
2024-06-26 10:54:57 +0300

    Update prime_numbers.py

--- Day2/prime_numbers.py
+++ Day2/prime_numbers.py
@@ -1,5 +1,5 @@
 def is_prime( n: int ):
-    list = [2,3,5,7,9,11]
+    list = [2,3,5,7,11]
     
     if n < 2:
         return False
@@ -15,9 +15,7 @@ n = int(input("Enter a number: "))
 
 counter= 0
 
-for y in range(1, n):
+for y in range(1, n+1):
     if (is_prime(y)):
-        counter = counter + 1
-        print(y)
-
+        counter += 1
 print(counter)
